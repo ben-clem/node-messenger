9c2bd1e4a4d607499662d6f94ec9a059219c47ea
Benoît Clemenceau <b.clemenceau.pro@gmail.com>
2020-11-25 03:10:01 +0100

    Redirect URL generation + WIP code_grant

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,36 +1,39 @@
-import {useState} from 'react'
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx } from "@emotion/core";
 // Local
-import Footer from './Footer'
-import Header from './Header'
-import Main from './Main'
-import Login from './Login'
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Login from "./Login";
 
 const styles = {
   root: {
-    boxSizing: 'border-box',
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#565E71',
-    padding: '50px',
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#565E71",
+    padding: "50px",
   },
-}
+};
 
 export default () => {
-  const [user, setUser] = useState(null)
-  const [drawerMobileVisible, setDrawerMobileVisible] = useState(false)
+  const [user, setUser] = useState(null);
+  const [drawerMobileVisible, setDrawerMobileVisible] = useState(false);
   const drawerToggleListener = () => {
-    setDrawerMobileVisible(!drawerMobileVisible)
-  }
+    setDrawerMobileVisible(!drawerMobileVisible);
+  };
+
   return (
     <div className="App" css={styles.root}>
-      <Header drawerToggleListener={drawerToggleListener}/>
-      {
-        user ? <Main drawerMobileVisible={drawerMobileVisible} /> : <Login onUser={setUser} />
-      }
+      <Header drawerToggleListener={drawerToggleListener} />
+      {user ? (
+        <Main drawerMobileVisible={drawerMobileVisible} />
+      ) : (
+        <Login onUser={setUser} />
+      )}
       <Footer />
     </div>
   );
-}
+};
--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,53 +1,145 @@
-import {} from 'react';
+import { useState } from "react";
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx } from "@emotion/core";
 // Layout
-import { useTheme } from '@material-ui/core/styles';
+import { useTheme } from "@material-ui/core/styles";
+//import { set } from "../../back-end/lib/app";
+const axios = require("axios");
+const qs = require("qs");
 
 const useStyles = (theme) => ({
   root: {
-    flex: '1 1 auto',
+    flex: "1 1 auto",
     background: theme.palette.background.default,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    '& > div': {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    "& > div": {
       margin: `${theme.spacing(1)}`,
-      marginLeft: 'auto',
-      marginRight: 'auto',
+      marginLeft: "auto",
+      marginRight: "auto",
     },
-    '& fieldset': {
-      border: 'none',
-      '& label': {
-        marginBottom: theme.spacing(.5),
-        display: 'block',
+    "& fieldset": {
+      border: "none",
+      "& label": {
+        marginBottom: theme.spacing(0.5),
+        display: "block",
       },
     },
   },
-})
+});
 
-export default ({
-  onUser
-}) => {
-  const styles = useStyles(useTheme())
+const crypto = require("crypto");
+
+const base64URLEncode = (str) =>
+  str
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+
+const sha256 = (buffer) => crypto.createHash("sha256").update(buffer).digest();
+
+const redirect_url = function (
+  authorization_endpoint,
+  client_id,
+  redirect_uri,
+  scope
+) {
+  const code_verifier = base64URLEncode(crypto.randomBytes(32));
+  const code_challenge = base64URLEncode(sha256(code_verifier));
+  const url = [
+    `${authorization_endpoint}?`,
+    `client_id=${client_id}&`,
+    `scope=${scope}&`,
+    "response_type=code&",
+    `redirect_uri=${redirect_uri}&`,
+    `code_challenge=${code_challenge}&`,
+    "code_challenge_method=S256",
+  ].join("");
+  const data = {
+    code_verifier,
+    url,
+  };
+  console.log(JSON.stringify(data, null, 2));
+
+  return data.code_verifier;
+};
+
+const code_grant = async function (code_verifier) {
+  // If this is a callback, POST code_grant
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const code = urlParams.get("code");
+
+  if (code !== null && code !== "") {
+    try {
+      const requestBody = qs.stringify({
+        grant_type: "authorization_code",
+        client_id: "node-messenger",
+        redirect_uri: 'http://127.0.0.1:3000/callback',
+        client_secret: "ZXhhbXBsZS1hcHAtc2VjcmV0",
+        code_verifier: code_verifier,
+        code: code,
+      }, { encode: false });
+      console.log(requestBody);
+
+      const { data: message } = await axios.post(
+        "http://127.0.0.1:5556/dex/token",
+        requestBody
+      );
+      console.log(JSON.stringify(message, null, 2));
+    } catch (err) {
+      console.log(JSON.stringify(err, null, 2));
+    }
+  }
+};
+
+export default ({ onUser }) => {
+  const styles = useStyles(useTheme());
+  const [codeVerifier, setCodeVerifier] = useState("");
+
+  code_grant(codeVerifier);
+
   return (
     <div css={styles.root}>
       <div>
-        <fieldset>
+        {/* <fieldset>
           <label htmlFor="username">username: </label>
           <input id="username" name="username" />
         </fieldset>
         <fieldset>
           <label htmlFor="password">password:</label>
           <input id="password" name="password" type="password" />
-        </fieldset>
+        </fieldset> */}
         <fieldset>
+          <input
+            type="submit"
+            value="login"
+            onClick={(e) => {
+              e.stopPropagation();
+
+              setCodeVerifier(
+                redirect_url(
+                  "http://127.0.0.1:5556/dex/auth",
+                  "node-messenger",
+                  "http://127.0.0.1:3000/callback",
+                  "openid%20email%20offline_access"
+                )
+              );
+
+              //onUser({username: 'david'})
+            }}
+          />
+        </fieldset>
+        {/* <fieldset>
           <input type="submit" value="login" onClick={ (e) => {
             e.stopPropagation()
             onUser({username: 'david'})
           }} />
-        </fieldset>
+        </fieldset> */}
       </div>
     </div>
   );
-}
+};
